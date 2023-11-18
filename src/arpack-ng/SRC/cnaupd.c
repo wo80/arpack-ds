52daf841f369bb95b89e679612ca152f8c2961a5
wo80 <christian.woltering@tu-dortmund.de>
2023-11-18 15:01:10 +0100

    Remove top banner inserted by f2c.

--- src/arpack-ng/SRC/cnaupd.c
+++ src/arpack-ng/SRC/cnaupd.c
@@ -1,14 +1,4 @@
-/* D:\projects\Fortran\arpack-ng-3.9.1-patched\SRC\cnaupd.f -- translated by f2c (version 20230428).
-   You must link the resulting object file with libf2c:
-    on Microsoft Windows system, link with libf2c.lib;
-    on Linux or Unix systems, link with .../path/to/libf2c.a -lm
-    or, if you install libf2c.a in a standard place, with -lf2c -lm
-    -- in that order, at the end of the command line, as in
-        cc *.o -lf2c -lm
-    Source for libf2c is in /netlib/f2c/libf2c.zip, e.g.,
-
-        http://www.netlib.org/f2c/libf2c.zip
-*/
+/* SRC\cnaupd.f -- translated by f2c (version 20230428). */
 
 #include "f2c.h"
 
