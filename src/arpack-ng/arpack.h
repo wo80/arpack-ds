50db491dcd26a99091c697bac786833acaccda1d
wo80 <christian.woltering@tu-dortmund.de>
2023-11-20 12:20:38 +0100

    Add CMake build files.

--- src/arpack-ng/arpack.h
+++ src/arpack-ng/arpack.h
@@ -2,6 +2,21 @@
 
 #include "arpack_types.h"
 
+/* ARPACK legacy version */
+#define ARPACK_VERSION "2.4.0"
+#define ARPACK_DATE "07/31/1996"
+
+#define ARPACK_VERSION_MAJOR 2
+#define ARPACK_VERSION_MINOR 4
+#define ARPACK_VERSION_PATCH 0
+
+/* arpack-ng version */
+#define arpack_ng_VERSION "3.9.1"
+
+#define arpack_ng_VERSION_MAJOR 3
+#define arpack_ng_VERSION_MINOR 9
+#define arpack_ng_VERSION_PATCH 1
+
 #ifdef __cplusplus
 extern "C"
 {
